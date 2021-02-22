7baffd97e01de7e14a5e28dba3801590fbbd8b72
Sophia <soph@soph.codes>
2021-02-22 12:06:57 -0500

    Testing out GET and POST APIs

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from pydub import AudioSegment
 app = Flask(__name__)
 
@@ -10,13 +10,19 @@ def match_target_amplitude(sound, target_dBFS):
 def index():
     return 'Index Page'
 
-@app.route("/normalize", methods=['POST'])    
+@app.route("/normalize/<audio_id>", methods=['GET','POST','DELETE'])    
 def normalize():
+    if request.method == 'GET':
+        data = request.form
+        return jsonify(isError=False,
+        message = "Success",
+        statusCode=200,
+        data = data), 200
     if request.method == 'POST':
         sound = AudioSegment.from_file("test.wav", "wav")
         normalized_sound = match_target_amplitude(sound, -20.0)
         normalized_sound.export("normalized_test.wav", format="wav")
-    return 'Normalization script'
+        return 'Normalization script'
 
 if  __name__ == "__main__":
     app.run()
