12599a8f59d96a5d1dfdaf99388daf68493fd358
Sophia <soph@soph.codes>
2021-02-22 10:34:56 -0500

    Modifying routes

--- app.py
+++ app.py
@@ -2,9 +2,13 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "Hello World!"
+def index():
+    return "Index Page"
 
-if  __name__ == "__maine__":
+@app.route("/normalize")
+def normalize():
+    return 'Normalization script'
+
+if  __name__ == "__main__":
     app.run()
 
