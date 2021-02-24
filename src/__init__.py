2c1e65ac9780ab16dd58bec8fe918823cc1bb748
Sophia <soph@soph.codes>
2021-02-24 17:26:08 -0500

    Modified environment variables

--- src/__init__.py
+++ src/__init__.py
@@ -1,3 +1,5 @@
+import os
+import sys
 from flask import Flask, jsonify
 from flask_restx import Resource, Api
 
@@ -6,7 +8,10 @@ app = Flask(__name__)
 api = Api(app)
 
 #set config
-app.config.from_object('src.config.DevelopmentConfig')
+app_settings = os.getenv('APP_SETTINGS')
+app.config.from_object(app_settings)
+
+#print(app.config, file=sys.stderr)
 
 class Ping(Resource):
     def get(self):
