c453f5e4663481d81f6e5513858742b9f361700d
Sophia <soph@soph.codes>
2021-02-24 18:50:23 -0500

    Initialized postgres database and updated Dockerfile to include entrypoint

--- Dockerfile
+++ Dockerfile
@@ -16,5 +16,6 @@ RUN pip install -r requirements.txt
 # add app
 COPY . .
 
-# run server
-CMD python manage.py run -h 0.0.0.0
+# add entrypoint.sh
+COPY ./entrypoint.sh .
+RUN chmod +x /usr/src/app/entrypoint.sh
--- src/__init__.py
+++ src/__init__.py
@@ -2,6 +2,7 @@ import os
 import sys
 from flask import Flask, jsonify
 from flask_restx import Resource, Api
+from flask_sqlalchemy import SQLAlchemy
 
 #instantiate app
 app = Flask(__name__)
@@ -11,7 +12,22 @@ api = Api(app)
 app_settings = os.getenv('APP_SETTINGS')
 app.config.from_object(app_settings)
 
+# instantiate the db
+db = SQLAlchemy(app)
+
 #print(app.config, file=sys.stderr)
+
+#model
+class File(db.Model):
+    __tablename__ = 'files'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(64))
+    fp = db.Column(db.String(264), unique=True) #absolute path
+    active = db.Column(db.Boolean(), default=True, nullable=False)
+
+    def __init__(self, name, fp):
+        self.name = name
+        self.fp = fp
 
 class Ping(Resource):
     def get(self):
