b253a845b0dbb8be6759243a95bc9f97f2fe073d
Sophia <soph@soph.codes>
2021-02-24 19:16:01 -0500

    Updated Dockerfile with database URLs, set up entrypoint, updated manage to recreate db

--- Dockerfile
+++ Dockerfile
@@ -9,6 +9,11 @@ WORKDIR /usr/src/app
 ENV PYTHONDONTWRITEBYTECODE 1
 ENV PYTHONUNBUFFERED 1
 
+# install system dependencies
+RUN apt-get update \
+    && apt-get -y install netcat gcc postgresql \
+    && apt-get clean
+
 # add and install requirements
 COPY ./requirements.txt .
 RUN pip install -r requirements.txt
--- manage.py
+++ manage.py
@@ -1,7 +1,14 @@
 from flask.cli import FlaskGroup
-from src import app
+from src import app, db
 
 cli = FlaskGroup(app)
+
+# registers recreate_db command for CLI
+@cli.command('recreate_db')
+def recreate_db():
+    db.drop_all()
+    db.create_all()
+    db.session.commit()
 
 if __name__ == '__main__':
     cli()
