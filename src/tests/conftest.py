cca176686154e17c031bf1f112851cc9cac14b84
Sophia <soph@soph.codes>
2021-02-24 19:29:10 -0500

    Added fixtures to testing configuration

---
+++ src/tests/conftest.py
@@ -0,0 +1,16 @@
+import pytest
+
+from src import app, db
+
+@pytest.fixture(scope='module')
+def test_app():
+    app.config.from_object('src.config.TestingConfig')
+    with app.app_context():
+        yield app # testing happens here
+
+@pytest.fixture(scope='module')
+def test_database():
+    db.create_all()
+    yield db #testing happens here
+    db.session.remove()
+    db.drop_all()
